b34ded4f17593bf7d266de6d72f8ef6010efe325
Harsh <89077517+waliaharsh74@users.noreply.github.com>
2022-05-17 07:54:00 +0530

    fun parallex animation

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+91 8077014424</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>harshwalia449@gmail.com</span>
+          </div>
+          <div className="itemContainer ">
+                  
+                  <a href="#" className="resume">Resume</a>
+                </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+// Icon made by Freepik from www.flaticon.com
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
